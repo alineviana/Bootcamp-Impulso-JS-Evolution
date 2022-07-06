693da74b659bd42d3edeb1bf4310bd4a0f5d7159
Aline Cristina Viana da Costa <alinecristinaviana@gmail.com>
2022-07-06 13:47:01 -0300

    add files

---
+++ JavaScript/Debugging e Error Handling/script.js
@@ -0,0 +1,42 @@
+// Atividade: validação de erros por tipo
+// Função recebe um array e retorna ele caso o seu tamanho corresponda ao número enviado como parâmetro na função.
+// Caso contrário, um erro será lançado.
+
+// Crie uma função que recebe um array e um número
+// Realize as seguintes validações:
+// 1) Se os parâmetros não forem enviados, lance um erro do tipo ReferenceError
+// 2) Se o array não for do tipo 'object', lance um erro do tipo TypeError
+// 3) Se o número não for do tipo 'number', lance um erro do tipo TypeError
+// 4) Se o tamanho do array for diferente do número enviado como parâmetro, lance um erro do tipo RangeError
+// Utilize a declaração try...catch
+// Filtre as chamadas de catch por cada tipo de erro utilizando o operador instanceof
+
+function validaArrays(arr, num) {
+  try {
+    if (!arr && !num) throw new ReferenceError("Envie os parâmetros!");
+
+    if (typeof arr !== 'object')
+      throw new TypeError("Envie um elemento do tipo array!");
+
+    if (typeof num !== 'number')
+      throw new TypeError("Envie um elemento do tipo number!");
+
+    if (arr.length !== num)
+      throw new RangeError("Tamanho do array inválido!");
+
+    return arr;
+  } catch (e) {
+    if (e instanceof RangeError) {
+      console.log("RangeError!");
+      console.log(e.stack);
+    } else if (e instanceof ReferenceError) {
+      console.log("ReferenceError!");
+      console.log(e.stack);
+    } else {
+      console.log("Outro tipo de erro!");
+      console.log(e.stack);
+    }
+  }
+}
+
+console.log(validaArrays([], a));
--- README.md
+++ README.md
@@ -1,25 +1,24 @@
-
 <h1> 
 	🚧 🚀 Em construção...  🚧
 </h1>
 
-<h1 align=center> Bootcamp Impulso JavaScript Evolution</h1>
+<h1> Bootcamp Impulso JavaScript Evolution</h1>
 
-<p align=center>
-<img src="./img/bootcamp-impulso.png" alt="logo" width=150px align="middle">
-<img src="./img/logo.svg" width=250px align="middle">
+<p>
+<img src="./Imagens/bootcamp-impulso.png" alt="logo" width=150px align="middle">
+<img src="./Imagens/logo.svg" width=250px align="middle">
 </p>
 
 # Descrição do projeto
+
 Bootcamp promovido pela DIO e pela Impulso com foco na evolução Fullstack.
 
+
 # Módulos
+
 - ✅ Módulo I - Princípios de Desenvolvimento de Software
-- ✅ Módulo II - Fundamentos Web com HTML e CSS 
+- ✅ Módulo II - Fundamentos Web com HTML e CSS
 - <img src="https://img.shields.io/badge/status-em%20andamento-green"> ✅ Módulo III - Dominando a Linguagem de Programação JavaScript
 - <img src="https://img.shields.io/badge/status-em%20andamento-green"> ✅ Módulo IV - Front-end: Desenvolvimento Web com React
-- <img src="https://img.shields.io/badge/status-em%20andamento-green"> ✅ Módulo V - Banco de Dados Relacionais(SQL) e Não Relacionais(NoSQL) 
+- <img src="https://img.shields.io/badge/status-em%20andamento-green"> ✅ Módulo V - Banco de Dados Relacionais(SQL) e Não Relacionais(NoSQL)
 - <img src="https://img.shields.io/badge/status-em%20andamento-green"> ✅ Módulo VI - Back-end: Criando API com Node.Js
-
-
-
